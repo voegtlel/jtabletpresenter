4214ebb1c588ffd3133da5f75214ba4d34a7f70a
lukas <voegtlel@tf.uni-freiburg.de>
2012-11-01 19:51:48 +0100

    Added button function for load/save

--- src/de/freiburg/uni/tablet/presenter/editor/toolpageeditor/buttons/ButtonOpenFrom.java
+++ src/de/freiburg/uni/tablet/presenter/editor/toolpageeditor/buttons/ButtonOpenFrom.java
@@ -5,7 +5,18 @@
 package de.freiburg.uni.tablet.presenter.editor.toolpageeditor.buttons;
 
 import java.awt.Component;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
+
+import de.freiburg.uni.tablet.presenter.data.BinaryDeserializer;
+import de.freiburg.uni.tablet.presenter.document.Document;
 import de.freiburg.uni.tablet.presenter.editor.IToolPageEditor;
 
 /**
@@ -22,6 +33,58 @@ public class ButtonOpenFrom extends AbstractButtonAction {
 
 	@Override
 	public void perform(final Component button) {
-		// _editor.deserialize();
+		JFileChooser fileChooser = new JFileChooser();
+		FileFilter presenterDocumentFile = new FileFilter() {
+			@Override
+			public String getDescription() {
+				return "JPresenter Document File (*.jpd)";
+			}
+			
+			@Override
+			public boolean accept(File f) {
+				return f.getPath().toLowerCase().endsWith(".jpd");
+			}
+		};
+		FileFilter presenterPageFile = new FileFilter() {
+			@Override
+			public String getDescription() {
+				return "JPresenter Page File (*.jpp)";
+			}
+			
+			@Override
+			public boolean accept(File f) {
+				return f.getPath().toLowerCase().endsWith(".jpp");
+			}
+		};
+		FileFilter pdf = new FileFilter() {
+			@Override
+			public String getDescription() {
+				return "PDF (*.pdf)";
+			}
+			
+			@Override
+			public boolean accept(File f) {
+				return f.getPath().toLowerCase().endsWith(".pdf");
+			}
+		};
+		fileChooser.addChoosableFileFilter(presenterDocumentFile);
+		fileChooser.addChoosableFileFilter(presenterPageFile);
+		fileChooser.addChoosableFileFilter(pdf);
+		fileChooser.setFileFilter(presenterDocumentFile);
+		if (fileChooser.showOpenDialog(button) == JFileChooser.APPROVE_OPTION) {
+			File f = fileChooser.getSelectedFile();
+			try {
+				FileInputStream fileInputStream = new FileInputStream(f);
+				BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+				BinaryDeserializer binaryDeserializer = new BinaryDeserializer(bufferedInputStream);
+				_editor.getDocumentEditor().setDocument(new Document(binaryDeserializer));
+				bufferedInputStream.close();
+				fileInputStream.close();
+			} catch (FileNotFoundException e) {
+				JOptionPane.showMessageDialog(button, "Couldn't open file", "Error", JOptionPane.ERROR_MESSAGE);
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(button, "Couldn't read file: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			}
+		}
 	}
 }
--- src/de/freiburg/uni/tablet/presenter/editor/toolpageeditor/buttons/ButtonSaveAs.java
+++ src/de/freiburg/uni/tablet/presenter/editor/toolpageeditor/buttons/ButtonSaveAs.java
@@ -5,7 +5,19 @@
 package de.freiburg.uni.tablet.presenter.editor.toolpageeditor.buttons;
 
 import java.awt.Component;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import javax.print.attribute.standard.JobMessageFromOperator;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
+
+import de.freiburg.uni.tablet.presenter.data.BinarySerializer;
 import de.freiburg.uni.tablet.presenter.editor.IToolPageEditor;
 
 /**
@@ -22,6 +34,58 @@ public class ButtonSaveAs extends AbstractButtonAction {
 
 	@Override
 	public void perform(final Component button) {
-		// _editor.serialize();
+		JFileChooser fileChooser = new JFileChooser();
+		FileFilter presenterDocumentFile = new FileFilter() {
+			@Override
+			public String getDescription() {
+				return "JPresenter Document File (*.jpd)";
+			}
+			
+			@Override
+			public boolean accept(File f) {
+				return f.getPath().toLowerCase().endsWith(".jpd");
+			}
+		};
+		FileFilter presenterPageFile = new FileFilter() {
+			@Override
+			public String getDescription() {
+				return "JPresenter Page File (*.jpp)";
+			}
+			
+			@Override
+			public boolean accept(File f) {
+				return f.getPath().toLowerCase().endsWith(".jpp");
+			}
+		};
+		FileFilter pdf = new FileFilter() {
+			@Override
+			public String getDescription() {
+				return "PDF (*.pdf)";
+			}
+			
+			@Override
+			public boolean accept(File f) {
+				return f.getPath().toLowerCase().endsWith(".pdf");
+			}
+		};
+		fileChooser.addChoosableFileFilter(presenterDocumentFile);
+		fileChooser.addChoosableFileFilter(presenterPageFile);
+		fileChooser.addChoosableFileFilter(pdf);
+		fileChooser.setFileFilter(presenterDocumentFile);
+		if (fileChooser.showSaveDialog(button) == JFileChooser.APPROVE_OPTION) {
+			File f = fileChooser.getSelectedFile();
+			try {
+				FileOutputStream fileOutputStream = new FileOutputStream(f);
+				BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+				BinarySerializer binarySerializer = new BinarySerializer(bufferedOutputStream);
+				_editor.getDocumentEditor().getDocument().serialize(binarySerializer);
+				bufferedOutputStream.close();
+				fileOutputStream.close();
+			} catch (FileNotFoundException e) {
+				JOptionPane.showMessageDialog(button, "Couldn't open file", "Error", JOptionPane.ERROR_MESSAGE);
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(button, "Couldn't write file: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			}
+		}
 	}
 }
