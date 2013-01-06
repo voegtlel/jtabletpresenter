2fccc368cf30cb3a9efd7f3e18f8736669dda51a
voegtlel <voegtlel@tf.uni-freiburg.de>
2013-01-06 02:40:22 +0100

    Changed initial size

--- src/de/freiburg/uni/tablet/presenter/TestApp.java
+++ src/de/freiburg/uni/tablet/presenter/TestApp.java
@@ -41,7 +41,7 @@ public class TestApp {
 	 */
 	private void initialize() {
 		_pageRenderer = new JPageEditor();
-		_pageRenderer.setBounds(100, 100, 450, 300);
+		_pageRenderer.setBounds(100, 100, 640, 480);
 		_pageRenderer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		_pageRenderer.setDocumentEditor(new DocumentEditor());
 
