2727dc01e8941ab49cfa5050824f5eec2aa34439
Lukas <lukas@infornautik.de>
2015-12-08 17:15:06 +0100

    v1.25 Added Updater.jar to ignored files in packed Fixed updater error

--- src/de/freiburg/uni/tablet/presenter/Updater.java
+++ src/de/freiburg/uni/tablet/presenter/Updater.java
@@ -304,13 +304,13 @@ public class Updater extends JDialog {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			File inputFile = new File(args[0]);
-			File outputFolder = new File(args[1]);
-			File selfFile = new File(args[2]);
+			final File inputFile = new File(args[0]);
+			final File outputFolder = new File(args[1]);
+			final File selfFile = new File(args[2]);
 			//String classPath = args[3];
 			//String mainClassName = args[4];
-			File jarFile = new File(outputFolder, "JTabletPresenter.jar");
-			String[] mainClassArgs = new String[args.length - 5];
+			final File jarFile = new File(outputFolder, "JTabletPresenter.jar");
+			final String[] mainClassArgs = new String[args.length - 5];
 			for (int i = 0; i < mainClassArgs.length; i++) {
 				mainClassArgs[i] = args[i + 5];
 			}
